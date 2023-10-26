be4390c3d3bd50608f01c46428d068af32a3430a
Alejandro Gomez <alexserverone@gmail.com>
2023-10-26 15:36:42 -0600

    removing some config from prettierrc since the VSCode extension contains already the pretier plugin for astro, need to read more about this

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,14 +2,6 @@
 /** @type {import("prettier").Config} */
 export default {
     plugins: ['prettier-plugin-astro'],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro',
-        },
-      },
-    ],
     useTabs: false,
     semi: false,
     tabWidth: 2,
