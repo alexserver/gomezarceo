20dd4bf6e9cdff206e26134d42998fdd42a5c46d
Alejandro Gomez <alexserverone@gmail.com>
2023-10-26 15:00:25 -0600

    changing prettier file

---
+++ .prettierrc.mjs
@@ -0,0 +1,17 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+      {
+        files: '*.astro',
+        options: {
+          parser: 'astro',
+        },
+      },
+    ],
+    useTabs: false,
+    semi: false,
+    tabWidth: 2,
+    singleQuote: true,
+  };
